3a7776e145196484d74060475439a8d48e577e4c
Farjan Hasan <farjanhasannias@gmail.com>
2024-06-26 09:50:36 +0600

    first commit

---
+++ edit-teacher.php
@@ -0,0 +1,272 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+if(strlen($_SESSION['alogin'])=="")
+    {   
+    header("Location: index.php"); 
+    }
+    else{
+
+$id=intval($_GET['teacher_id']);
+
+if(isset($_POST['submit']))
+{
+
+$fullanme=$_POST['fullanme'];
+$teacher_id=$_POST['teacher_id'];
+$designation=$_POST['designation'];
+$phone=$_POST['phone'];
+$email=$_POST['emailid'];
+$password=$_POST['password'];
+$gender=$_POST['gender'];
+$dob =$_POST['dob'];
+$status=$_POST['status'];
+
+$sql="update tblstudents set fullanme=:fullanme,teacher_id=:teacher_id,designation=:designation,phone=:phone,email=:email,password=:password,gender=:gender,dob=:dob,status=:status where id=:id";
+$query = $dbh->prepare($sql);
+$query->bindParam(':fullanme',$fullanme,PDO::PARAM_STR);
+$query->bindParam(':teacher_id',$teacher_id,PDO::PARAM_STR);
+$query->bindParam(':designation',$designation,PDO::PARAM_STR);
+$query->bindParam(':phone',$phone,PDO::PARAM_STR);
+$query->bindParam(':email',$email,PDO::PARAM_STR);
+$query->bindParam(':password',$password,PDO::PARAM_STR);
+$query->bindParam(':gender',$gender,PDO::PARAM_STR);
+$query->bindParam(':dob',$dob,PDO::PARAM_STR);
+$query->bindParam(':status',$status,PDO::PARAM_STR);
+$query->bindParam(':id',$id,PDO::PARAM_STR);
+$query->execute();
+
+$msg="Teacher info updated successfully";
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>SMS Admin| Edit Teacher < </title>
+        <link rel="stylesheet" href="css/bootstrap.min.css" media="screen" >
+        <link rel="stylesheet" href="css/font-awesome.min.css" media="screen" >
+        <link rel="stylesheet" href="css/animate-css/animate.min.css" media="screen" >
+        <link rel="stylesheet" href="css/lobipanel/lobipanel.min.css" media="screen" >
+        <link rel="stylesheet" href="css/prism/prism.css" media="screen" >
+        <link rel="stylesheet" href="css/select2/select2.min.css" >
+        <link rel="stylesheet" href="css/main.css" media="screen" >
+        <script src="js/modernizr/modernizr.min.js"></script>
+    </head>
+    <body class="top-navbar-fixed">
+        <div class="main-wrapper">
+
+            <!-- ========== TOP NAVBAR ========== -->
+  <?php include('includes/topbar.php');?> 
+            <!-- ========== WRAPPER FOR BOTH SIDEBARS & MAIN CONTENT ========== -->
+            <div class="content-wrapper">
+                <div class="content-container">
+
+                    <!-- ========== LEFT SIDEBAR ========== -->
+                   <?php include('includes/leftbar.php');?>  
+                    <!-- /.left-sidebar -->
+
+                    <div class="main-page">
+
+                     <div class="container-fluid">
+                            <div class="row page-title-div">
+                                <div class="col-md-6">
+                                    <h2 class="title">Teacher Admission</h2>
+                                
+                                </div>
+                                
+                                <!-- /.col-md-6 text-right -->
+                            </div>
+                            <!-- /.row -->
+                            <div class="row breadcrumb-div">
+                                <div class="col-md-6">
+                                    <ul class="breadcrumb">
+                                        <li><a href="dashboard.php"><i class="fa fa-home"></i> Home</a></li>
+                                
+                                        <li class="active">Teacher Admission</li>
+                                    </ul>
+                                </div>
+                             
+                            </div>
+                            <!-- /.row -->
+                        </div>
+                        <div class="container-fluid">
+                           
+                        <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="panel">
+                                            <div class="panel-heading">
+                                                <div class="panel-title">
+                                                    <h5>Fill the Teacher info</h5>
+                                                </div>
+                                            </div>
+                                            <div class="panel-body">
+<?php if($msg){?>
+<div class="alert alert-success left-icon-alert" role="alert">
+ <strong>Well done!</strong><?php echo htmlentities($msg); ?>
+ </div><?php } 
+else if($error){?>
+    <div class="alert alert-danger left-icon-alert" role="alert">
+                                            <strong>Oh snap!</strong> <?php echo htmlentities($error); ?>
+                                        </div>
+                                        <?php } ?>
+                                                <form class="form-horizontal" method="post">
+<?php 
+
+$sql = "SELECT * from teachers where id=:id";
+$query = $dbh->prepare($sql);
+$query->bindParam(':id',$id,PDO::PARAM_STR);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+$cnt=1;
+if($query->rowCount() > 0)
+{
+foreach($results as $result)
+{  ?>
+
+
+<div class="form-group">
+<label for="default" class="col-sm-2 control-label">Full Name</label>
+<div class="col-sm-10">
+<input type="text" name="fullanme" class="form-control" id="fullanme" value="<?php echo htmlentities($result->fullanme)?>" required="required" autocomplete="off">
+</div>
+</div>
+
+<div class="form-group">
+<label for="default" class="col-sm-2 control-label">Teacher's ID</label>
+<div class="col-sm-10">
+<input type="text" name="teacher_id" class="form-control" id="teacher_id" value="<?php echo htmlentities($result->teacher_id)?>" required="required" autocomplete="off">
+</div>
+</div>
+
+<div class="form-group">
+    <label for="default" class="col-sm-2 control-label">Email ID</label>
+    <div class="col-sm-10">
+    <input type="email" name="emailid" class="form-control" id="email" value="<?php echo htmlentities($result->email)?>" required="required" autocomplete="off">
+    </div>
+</div>
+
+<div class="form-group">
+    <label for="password" class="col-sm-2 control-label">Password</label>
+    <div class="col-sm-10">
+    <input type="text" name="password" class="form-control" id="password" value="" required="required" autocomplete="off">
+    </div>
+</div>
+
+<div class="form-group">
+    <label for="designation" class="col-sm-2 control-label">Designation</label>
+    <div class="col-sm-10">
+    <input type="text" name="designation" class="form-control" id="designation" value="<?php echo htmlentities($result->designation)?>" required="required" autocomplete="off">
+    </div>
+</div>
+
+<div class="form-group">
+    <label for="phone" class="col-sm-2 control-label">Phone</label>
+    <div class="col-sm-10">
+    <input type="text" name="phone" class="form-control" id="phone" value="<?php echo htmlentities($result->phone)?>" required="required" autocomplete="off">
+    </div>
+</div>
+
+<div class="form-group">
+<label for="default" class="col-sm-2 control-label">Gender</label>
+<div class="col-sm-10">
+<?php  $gndr=$result->gender;
+if($gndr=="Male")
+{
+?>
+<input type="radio" name="gender" value="Male" required="required" checked>Male <input type="radio" name="gender" value="Female" required="required">Female <input type="radio" name="gender" value="Other" required="required">Other
+<?php }?>
+<?php  
+if($gndr=="Female")
+{
+?>
+<input type="radio" name="gender" value="Male" required="required" >Male <input type="radio" name="gender" value="Female" required="required" checked>Female <input type="radio" name="gender" value="Other" required="required">Other
+<?php }?>
+<?php  
+if($gndr=="Other")
+{
+?>
+<input type="radio" name="gender" value="Male" required="required" >Male <input type="radio" name="gender" value="Female" required="required">Female <input type="radio" name="gender" value="Other" required="required" checked>Other
+<?php }?>
+
+
+</div>
+</div>
+
+
+<div class="form-group">
+                                                        <label for="date" class="col-sm-2 control-label">DOB</label>
+                                                        <div class="col-sm-10">
+                <input type="date"  name="dob" class="form-control" value="<?php echo htmlentities($result->dob)?>" id="date">
+                                                        </div>
+                                                    </div>
+
+<div class="form-group">
+<label for="default" class="col-sm-2 control-label">Status</label>
+<div class="col-sm-10">
+<?php  $stats=$result->status;
+if($stats=="1")
+{
+?>
+<input type="radio" name="status" value="1" required="required" checked>Active <input type="radio" name="status" value="0" required="required">Block 
+<?php }?>
+<?php  
+if($stats=="0")
+{
+?>
+<input type="radio" name="status" value="1" required="required" >Active <input type="radio" name="status" value="0" required="required" checked>Block 
+<?php }?>
+
+
+
+</div>
+</div>
+
+<?php }} ?>                                                    
+
+                                                    
+                                                    <div class="form-group">
+                                                        <div class="col-sm-offset-2 col-sm-10">
+                                                            <button type="submit" name="submit" class="btn btn-primary">Update</button>
+                                                        </div>
+                                                    </div>
+                                                </form>
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- /.col-md-12 -->
+                                </div>
+                    </div>
+                </div>
+                <!-- /.content-container -->
+            </div>
+            <!-- /.content-wrapper -->
+        </div>
+        <!-- /.main-wrapper -->
+        <script src="js/jquery/jquery-2.2.4.min.js"></script>
+        <script src="js/bootstrap/bootstrap.min.js"></script>
+        <script src="js/pace/pace.min.js"></script>
+        <script src="js/lobipanel/lobipanel.min.js"></script>
+        <script src="js/iscroll/iscroll.js"></script>
+        <script src="js/prism/prism.js"></script>
+        <script src="js/select2/select2.min.js"></script>
+        <script src="js/main.js"></script>
+        <script>
+            $(function($) {
+                $(".js-states").select2();
+                $(".js-states-limit").select2({
+                    maximumSelectionLength: 2
+                });
+                $(".js-states-hide").select2({
+                    minimumResultsForSearch: Infinity
+                });
+            });
+        </script>
+    </body>
+</html>
+<?PHP } ?>
---
+++ includes/leftbar.php
@@ -0,0 +1,70 @@
+<div class="left-sidebar bg-black-300 box-shadow ">
+    <div class="sidebar-content">
+        <div class="user-info closed">
+            <img src="images/user.png" alt="John Doe" class="img-circle profile-img">
+            <h6 class="title">a-coder</h6>
+            <small class="info">Envato Market Place</small>
+        </div>
+        <!-- /.user-info -->
+
+        <div class="sidebar-nav">
+            <ul class="side-nav color-gray">
+                <li class="nav-header">
+                    <span class="">Main Category</span>
+                </li>
+                <li>
+                    <a href="dashboard"><span class="glyphicon glyphicon-home"></span> <span> Home </span> </a>
+                 
+                </li>
+
+                <li class="nav-header">
+                    <span class="">Appearance</span>
+                </li>
+                <li class="has-children">
+                    <a href="#"><span class="glyphicon glyphicon-user"></span> <span>Students</span> <span class="glyphicon glyphicon-chevron-right"></span></a>
+                    <ul class="child-nav">
+                        <li><a href="add-students"><span class="glyphicon glyphicon-plus-sign"></span> <span>Add Students</span></a></li>
+                        <li><a href="manage-students"><span class="glyphicon glyphicon-cog"></span> <span>Manage Students</span></a></li>
+                       
+                    </ul>
+                </li>
+                <li class="has-children">
+                    <a href="#"><span class="glyphicon glyphicon-user"></span> <span>Teachers</span> <span class="glyphicon glyphicon-chevron-right"></span></a>
+                    <ul class="child-nav">
+                        <li><a href="add-teachers"><span class="glyphicon glyphicon-plus-sign"></span> <span>Add Teachers</span></a></li>
+                        <li><a href="manage-teachers"><span class="glyphicon glyphicon-cog"></span> <span>Manage Teachers</span></a></li>
+                       
+                    </ul>
+                </li>
+                <li class="has-children">
+                    <a href="#"><span class="glyphicon glyphicon-list-alt"></span> <span>Student Classes / Courses </span> <span class="glyphicon glyphicon-chevron-right"></span></a>
+                    <ul class="child-nav">
+                        <li><a href="create-class"><span class="glyphicon glyphicon-plus"></span> <span>Create Class / Course</span></a></li>
+                        <li><a href="manage-classes"><span class="glyphicon glyphicon-cog"></span> <span>Manage Classes / Courses</span></a></li>
+                       
+                    </ul>
+                </li>
+                <li class="has-children">
+                    <a href="#"><span class="glyphicon glyphicon-book"></span> <span>Subjects</span> <span class="glyphicon glyphicon-chevron-right"></span></a>
+                    <ul class="child-nav">
+                        <li><a href="create-subject"><span class="glyphicon glyphicon-plus"></span> <span>Create Subject</span></a></li>
+                        <li><a href="manage-subjects"><span class="glyphicon glyphicon-cog"></span> <span>Manage Subjects</span></a></li>
+                       <li><a href="add-subjectcombination"><span class="glyphicon glyphicon-plus-sign"></span> <span>Add Subject Combination </span></a></li>
+                       <a href="manage-subjectcombination"><span class="glyphicon glyphicon-cog"></span> <span>Manage Subject Combination </span></a></li>
+                    </ul>
+                </li>
+                <li class="has-children">
+                    <a href="#"><span class="glyphicon glyphicon-education"></span> <span>Result</span> <span class="glyphicon glyphicon-chevron-right"></span></a>
+                    <ul class="child-nav">
+                        <li><a href="add-result"><span class="glyphicon glyphicon-plus-sign"></span> <span>Add Result</span></a></li>
+                        <li><a href="manage-results"><span class="glyphicon glyphicon-cog"></span> <span>Manage Result</span></a></li>
+                       
+                    </ul>
+                    
+                </li>
+                <li><a href="change-password"><span class="glyphicon glyphicon-cog"></span> <span>Change Password</span></a></li>
+            </div>
+        <!-- /.sidebar-nav -->
+    </div>
+    <!-- /.sidebar-content -->
+</div>
